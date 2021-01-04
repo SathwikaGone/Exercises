52f12a249d158e2bd3826fe6d8d13744bb34ac77
SathwikaGone <gonesathwika@gmail.com>
2021-01-04 16:47:11 -0500

    ArrayList exercises

---
+++ JavaBasics/src/main/Exercises.java
@@ -0,0 +1,18 @@
+package main;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Exercises {
+
+	//Write a Java program to create a new array list, add some colors (string) and print out the collection.
+	public List<String> displayList() {
+		List<String> lists = new ArrayList<String>();
+		lists.add("green");
+		lists.add("pink");
+		return lists;
+				
+	}
+	
+	
+}
